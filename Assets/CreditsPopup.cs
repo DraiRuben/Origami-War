7a7e592c2063e3870ffec8946699869fa8c66f49
Roux Cédric <c.roux@lahorde.school>
2024-01-11 16:38:15 +0100

    Main Menu
    Main Menu
    Settings
    Credits

---
+++ Assets/CreditsPopup.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CreditsPopup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject Popup;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Popup.SetActive(true);   
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Popup.SetActive(false);   
+    }
+}
